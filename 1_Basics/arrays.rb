436c17c3cb110512f7e7d9da1797a4e39778c677
Cathy Raffaele <Cathy@fastmail.com.au>
2018-11-26 23:24:31 +1100

    Added Chapter 1 The Basics exercises

---
+++ 1_Basics/arrays.rb
@@ -0,0 +1,6 @@
+dates = [1998, 1990, 2001, 1986]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
---
+++ 1_Basics/movies.rb
@@ -0,0 +1,11 @@
+movies = {
+  heathers: 1988,
+  pump_up_the_volume: 1990,
+  zoolander: 2001,
+  room_with_a_view: 1986
+}
+
+puts movies[:heathers]
+puts movies[:pump_up_the_volume]
+puts movies[:zoolander]
+puts movies[:room_with_a_view]
