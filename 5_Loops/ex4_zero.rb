ee8552dd1fcdb564b9b048b940bc8741e0a4f49f
Cathy Raffaele <Cathy@fastmail.com.au>
2018-11-29 20:25:47 +1100

    Change Ch 5 ex 4 to include recursion

--- 5_Loops/ex4_zero.rb
+++ 5_Loops/ex4_zero.rb
@@ -1,16 +1,12 @@
 # Write a method that counts down to zero using recursion.
 
 def count_down(num)
-  if num > 0
-    while num >= 0
-      puts num
-      num -= 1
-    end
+  if num <= 0
+    puts num
   else
-    puts "Number needs to be more than zero"
+    puts num
+    count_down(num - 1)
   end
 end
 
-count_down(110)
-count_down(0)
-count_down(-10)
+count_down(10)
